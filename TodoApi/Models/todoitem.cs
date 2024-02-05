cf2258c9d179fc249bb1dfff93ca815e7c79b448
PauloCesarSJ <pcesar.sj2004@gmail.com>
2024-02-05 18:27:40 -0300

    models
    o folder models foi criado,  a class TodoItem foi criada, a classe TodoContex acaba de ser criada

---
+++ TodoApi/Models/todoitem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Models
+{
+    public class todoitem
+    {
+        public long id { get; set; }
+        public string? name { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
